6e110cac57193e577684a9e9d9880abc435859ff
Indrajeet Sisodiya <138592168+Indrajeetsisodiya@users.noreply.github.com>
2025-12-31 17:18:55 +0530

    Add type determination examples for integers, floats, and complex numbers
    Added examples to determine variable types in Python.

---
+++ numbers.py
@@ -0,0 +1,19 @@
+#1 determining type (integer)
+my_integer = 50
+print(type(my_integer))
+# output - <class 'int'>
+
+#2 determining type (float)
+my_float = 5.99 
+print(type(my_float))
+# output - <class 'float'>
+
+#3 determining type (complex)
+my_complex = 2j
+print(type(my_complex))
+# output - <class 'complex'>
+
+#ex-2 determining type (complex)
+hey_complex = 3 + 3j
+print(type(hey_complex))
+# output - <class 'complex'>
